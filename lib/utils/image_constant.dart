886f01b712b3e759f1a52cdc0104989e34a22928
amal-tech-dev <amalrofficial911@gmail.com>
2024-04-04 19:12:02 +0530

    Edited Home Screen

---
+++ lib/utils/image_constant.dart
@@ -0,0 +1,5 @@
+import 'package:shopping_app/generated/assets.dart';
+
+class ImageConstant {
+  static const menu = Assets.iconsMenu;
+}
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/utils/color_constant.dart';
+import 'package:shopping_app/utils/image_constant.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -20,7 +21,56 @@ class _HomeScreenState extends State<HomeScreen> {
             backgroundColor: Colors.grey,
           ),
         ),
-        actions: [],
+        actions: [
+          CircleAvatar(
+            radius: kToolbarHeight / 2 - 10,
+            backgroundColor: ColorConstant.tertiary,
+            foregroundImage: AssetImage(
+              ImageConstant.menu,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: ColorConstant.tertiary,
+        selectedItemColor: ColorConstant.primary,
+        unselectedItemColor: ColorConstant.primary,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(
+          color: ColorConstant.primary,
+          fontWeight: FontWeight.bold,
+        ),
+        items: [
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.add),
+            label: 'New Order',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.shopping_cart),
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.home_outlined),
+            label: 'Return Order',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(Icons.home_rounded),
+            icon: Icon(Icons.home_outlined),
+            label: 'Customers',
+          ),
+        ],
       ),
     );
   }
